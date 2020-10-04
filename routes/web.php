bfd4833bced5e00fa9cee5a7502890375392d65c
Muhammad Abdullah <ma22052000@gmail.com>
2020-10-04 18:49:09 +0700

    menambah route

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'AppController@index');
 Route::get('/tambah', 'HobiController@tambahHobi');
+Route::get('/update', 'HobiController@updateHobi');
